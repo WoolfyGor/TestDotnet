2d4b3f445d442d3a527a15820f5c9700bb3e4341
WoolfyGor <limoni10@yandex.ru>
2024-10-03 00:05:58 +0300

    final

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Net;
+using System.Numerics;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 8000);
+            Console.WriteLine("Server started");
+            serverSocket.Start();
+            while (true)
+            {
+                TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                NetworkStream stream = clientSocket.GetStream();
+                var player = new PlayerData();
+                player.score = 5;
+                string data = JsonSerializer.Serialize(player);
+                Console.WriteLine(data);
+                byte[] bytes = Encoding.ASCII.GetBytes(data); ;
+                stream.Write(bytes, 0, bytes.Length);
+                clientSocket.Close();
+            }
+            serverSocket.Stop();
+            Console.WriteLine("Server stopped");
+            Console.ReadLine();
+        }
+
+        [Serializable]
+        public class PlayerData
+        {
+            public Vector3 pos { get; set; }
+            public float score { get; set; }
+            public PlayerData()
+            {
+                pos = new Vector3(1, 1, 1);
+                score = 1.5f;
+            }
+        }
+    }
+}
